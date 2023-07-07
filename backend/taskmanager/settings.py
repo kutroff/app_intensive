96d7fa17bd53016f17d2a0cd5daec9bde83d0617
kutroff <nikolajkutrov@gmail.com>
2023-07-07 09:40:31 +0000

    поменялись settings

--- backend/taskmanager/settings.py
+++ backend/taskmanager/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-6-u^5lwyqvdj9ik@&5+vu276%sa%9uygd=)f)r@b)rudsiis8=
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['nikolai.pythonanywhere.com']
 
 # Application definition
 
@@ -129,6 +129,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+MEDIA_URL = '/media/'
+STATIC_ROOT = '/home/nikolai/static'
+MEDIA_ROOT = '/home/nikolai/media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
